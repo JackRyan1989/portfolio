55c93f65889128cfb186f4102c615f916a454057
JackRyan1989 <jack.jackryan@gmail.com>
2021-02-12 18:59:23 -0500

    more styling

---
+++ src/components/footer.svelte
@@ -0,0 +1,37 @@
+<script>
+</script>
+
+<section>
+  <div class="links">
+    <h2>Hit Me Up!</h2>
+    <a href="https://www.linkedin.com/in/johnanthonyryan/" target="_blank"
+      >LinkedIn</a
+    >
+    <a href="mailto:jack.jackryan@protonmail.com">Email</a>
+    <a href="https://github.com/JackRyan1989" target="_blank">Github</a>
+  </div>
+</section>
+
+<style>
+  section {
+    display: grid;
+    border: solid lightgray 1px;
+  }
+
+  div.links {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  div.links a {
+    padding: 0.5em;
+    margin: 0.5em;
+    min-height: 4em;
+    text-decoration: none;
+    text-align: center;
+    color: #000;
+    border-radius: 0.5em;
+  }
+</style>
--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,39 +1,110 @@
 <script>
-  import PROJECTS from '../constants/projects'
+  import PROJECTS from "../constants/projects";
 </script>
 
 <section>
   <h2>Projects</h2>
   <article>
-  {#each PROJECTS as project}
-    <div id={project.id}>
-      <h3>{project.title}</h3>
-      {@html project.role}
-      <a href={project.url} target="_blank"><i class="fab fa-github-square"></i> Github</a>
-    </div>
+    {#each PROJECTS as project}
+      <div class="item-grid" id={project.id}>
+        <a class="item" href={project.url} target="_blank">
+          <div class="item-content">
+            <div>
+              <h3 class="item-heading">{project.title}</h3>
+            </div>
+            <div>
+              <span class="item-category">{@html project.tech}</span>
+            </div>
+            <div>
+              {@html project.tasks}
+            </div>
+          </div>
+        </a>
+      </div>
     {:else}
-    <div>
+      <div>
         <p>Hey something went wrong loading projects!</p>
-    </div>
-  {/each}
-</article>
+      </div>
+    {/each}
+  </article>
 </section>
 
 <style>
+  :root {
+    --text-light: rgba(255, 255, 255, 0.6);
+    --text-lighter: rgba(255, 255, 255, 0.9);
+    --spacing-s: 8px;
+    --spacing-m: 16px;
+    --spacing-l: 24px;
+    --spacing-xl: 32px;
+    --spacing-xxl: 64px;
+    --width-container: 1200px;
+  }
 
-    section {
-        display: grid;
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    vertical-align: baseline;
+    background: transparent;
+  }
+
+  section {
+    display: grid;
+  }
+
+  article {
+    align-items: flex-start;
+    display: flex;
+    min-height: 100%;
+    justify-content: center;
+    padding: 0px var(--spacing-l) var(--spacing-xxl) var(--spacing-l);
+  }
+
+  .item-grid {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-column-gap: var(--spacing-l);
+    grid-row-gap: var(--spacing-l);
+    max-width: var(--width-container);
+    width: 100%;
+  }
+
+  .item {
+    position: relative;
+  }
+
+  .item:hover {
+    transform: scale(1.05) translateZ(0);
+  }
+
+  .item-grid:hover > .item:not(:hover) {
+    filter: brightness(0.5) saturate(0) contrast(1.2) blur(20px);
+  }
+
+    .item-category {
+      font-size: .85em;
+      font-style: italic;
     }
 
-    article {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        padding: .5em;
-    }
 
-    div {
-        padding: 1em;
-    }
+  .item-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-width: 25%;
+    padding: 0.5em;
+    margin: 0.5em;
+    min-height: 4em;
+    text-decoration: none;
+    border: solid lightgrey 1px;
+    color: #000;
+    border-radius: 0.5em;
+  }
 
+  a:hover {
+    text-decoration: none;
+  }
 </style>
